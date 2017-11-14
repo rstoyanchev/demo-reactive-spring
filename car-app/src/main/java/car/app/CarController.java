675e8567a0b1dbaea261cfccff095b28e973895c
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2017-11-14 14:03:49 -0800

    Simplify CarController

--- car-app/src/main/java/car/app/CarController.java
+++ car-app/src/main/java/car/app/CarController.java
@@ -21,7 +21,6 @@ import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -32,45 +31,29 @@ public class CarController {
 	private static final Logger logger = LoggerFactory.getLogger(CarController.class);
 
 
-	private final WebClient carLocationClient;
+	private final WebClient carsClient = WebClient.create("http://localhost:8081");
 
-	private final WebClient carRequestClient;
-
-
-	public CarController() {
-
-		ReactorClientHttpConnector connector = new ReactorClientHttpConnector();
-
-		carLocationClient = WebClient.builder()
-				.baseUrl("http://localhost:8081")
-				.clientConnector(connector)
-				.build();
-
-		carRequestClient = WebClient.builder()
-				.baseUrl("http://localhost:8082")
-				.clientConnector(connector)
-				.build();
-	}
+	private final WebClient bookClient = WebClient.create("http://localhost:8082");
 
 
 	@PostMapping("/booking")
 	public Mono<ResponseEntity<Void>> book() {
-
 		logger.debug("Processing booking request..");
-
-		return carLocationClient.get()
-				.uri("/cars")
+		return carsClient.get().uri("/cars")
 				.retrieve()
 				.bodyToFlux(Car.class)
-				.doOnNext(car -> logger.debug("Requesting to book " + car))
+				.doOnNext(car -> logger.debug("Trying to book " + car))
 				.take(5)
-				.flatMap(car ->
-						carRequestClient.post()
-								.uri("/cars/{id}/booking", car.getId())
-								.exchange()
-								.flatMap(response -> response.toEntity(Void.class)))
+				.flatMap(this::requestCar)
 				.next()
 				.doOnNext(car -> logger.debug("Booked car " + car));
+	}
+
+	private Mono<ResponseEntity<Void>> requestCar(Car car) {
+		return bookClient.post()
+				.uri("/cars/{id}/booking", car.getId())
+				.exchange()
+				.flatMap(response -> response.toEntity(Void.class));
 	}
 
 }
